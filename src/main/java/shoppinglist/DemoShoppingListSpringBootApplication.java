21f31371a75729fa9c92faa25aab0c8afde6a6e2
JulianElyK <julianely23@gmail.com>
2021-03-28 13:51:04 +0700

    initial commit

--- src/main/java/shoppinglist/DemoShoppingListSpringBootApplication.java
+++ src/main/java/shoppinglist/DemoShoppingListSpringBootApplication.java
@@ -1,5 +1,6 @@
 package shoppinglist;
 
+import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -43,13 +44,57 @@ public class DemoShoppingListSpringBootApplication implements CommandLineRunner
         System.out.print("Masukkan ID dari objek DaftarBelanja yg ingin ditampilkan: ");
         long id = Long.parseLong(keyb.nextLine());
         System.out.println("Hasil pencarian: ");
+        bacaBerdasarkanId(id);
         
+        // Mencari DaftarBelanja berdasarkan kemiripan judul
+        System.out.print("Masukkan judul DaftarBelanja yang ingin dicari: ");
+        String judul = keyb.nextLine();
+        DaftarBelanja db = new DaftarBelanja();
+        db.setJudul(judul);
+//        optDB = repo.findOne();
+        
+        System.out.println("Hasil");
+        
+        // Menyimpan DaftarBelanja
+        System.out.print("Masukkan judul DaftarBelanja: ");
+        judul = keyb.nextLine();
+        
+        db.setJudul(judul);
+        db.setTanggal(LocalDateTime.now());
+        
+        System.out.print("Masukkan jumlah barang dalam list: ");
+        int jmlh = Integer.parseInt(keyb.nextLine());
+        
+        for (int i = 1; i <= jmlh; i++) {
+            System.out.print("Masukkan nama barang " + i +": ");
+            String nama = keyb.nextLine();
+            
+            System.out.print("Masukkan banyak barang " + i +": ");
+            int bnyk = Integer.parseInt(keyb.nextLine());
+            
+            System.out.print("Masukkan satuan banyak barang " + i +": ");
+            String satuan = keyb.nextLine();
+            
+            System.out.print("Tulis memo barang " + i +": ");
+            String memo = keyb.nextLine();
+            
+            DaftarBelanjaDetil dbd = new DaftarBelanjaDetil();
+            dbd.setNoUrut(i);
+            dbd.setNamaBarang(nama);
+            dbd.setByk(bnyk);
+            dbd.setSatuan(satuan);
+            dbd.setMemo(memo);
+            
+            db.getDaftarBarang().add(dbd);
+        }
+    }
+    
+    private void bacaBerdasarkanId(long id){
         Optional<DaftarBelanja> optDB = repo.findById(id);
         if (optDB.isPresent()) {
             DaftarBelanja db = optDB.get();
             System.out.println("\tJudul: " + db.getJudul());
-        }
-        else {
+        } else {
             System.out.println("\tTIDAK DITEMUKAN.");
         }
     }
